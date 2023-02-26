f3b151f344b9bce7abddf328d503615cb0d08063
MeerkatBoss <ivan.solodovnikov@outlook.com>
2023-02-26 17:17:33 +0300

    feat: insertion sort full tests

--- runner.sh
+++ runner.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 make run BUILDTYPE=Release TEST_CASE=insertion_sort &&\
-    git add . &&\
-    git commit -m "feat: insertion sort tests"&&\
-    git push -u origin test_results &&\
+    git add . && git commit -m "feat: insertion sort full tests" && git push &&\
+make run BUILDTYPE=Release TEST_CASE=selection_sort &&\
+    git add . && git commit -m "feat: selection sort full tests" && git push &&\
+make run BUILDTYPE=Release TEST_CASE=bubble_sort &&\
+    gut add . && git commit -m "feat: bubble sort full tests" && git push &&\
     shutdown
 
 
