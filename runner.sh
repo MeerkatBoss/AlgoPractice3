78dd501035d02510085cb56b18b601328f6d4933
MeerkatBoss <ivan.solodovnikov@outlook.com>
2023-03-04 14:23:54 +0300

    feat: logarithmic sorts tests

--- runner.sh
+++ runner.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-make run BUILDTYPE=Release TEST_CASE=selection_sort &&\
-    git add . && git commit -m "feat: selection sort full tests" && git push &&\
-make run BUILDTYPE=Release TEST_CASE=bubble_sort &&\
-    gut add . && git commit -m "feat: bubble sort full tests" && git push &&\
+make run BUILDTYPE=Release TEST_CASE=log_sorts &&\
+    git add . && git commit -m "feat: logarithmic sorts tests" && git push &&\
     shutdown
 
 
